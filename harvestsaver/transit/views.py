99bc95d774e87766c74fcbe4f0a52c15bb4caf8a
Dennis Musembi Mwendwa <dennismusembi2@gmail.com>
2023-11-24 06:22:28 +0300

    created quote model for trant app

--- harvestsaver/transit/views.py
+++ harvestsaver/transit/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
-from .models import TransportBooking
+from .models import TransportBooking, Quote
 
 def transporthome(request):
+    if request.method == "POST":
+        pass
+    
     return render(request, "transit/transport.html", {})
