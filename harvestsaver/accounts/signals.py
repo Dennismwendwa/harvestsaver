bd011e615dbc17f5295ff338408ced0edbe943df
Dennis Musembi Mwendwa <dennismusembi2@gmail.com>
2023-11-27 16:55:30 +0300

    created payment app, and account model

--- harvestsaver/accounts/signals.py
+++ harvestsaver/accounts/signals.py
@@ -1,16 +1,20 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from accounts.models import User, Profile, FarmerProfile, BuyerProfile
-from accounts.models import EquipmentOwnerProfile
+from payment.models import Account, accounts_number
 
 
 @receiver(post_save, sender=User, dispatch_uid="create_profile")
 def create_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
+        Account.objects.create(user=instance,
+                               account_number=accounts_number(instance.pk),
+                               account_name= f"{instance.first_name} {instance.last_name}")
 
 
 @receiver(post_save, sender=User)
 def save_profile(sender, instance, **kwargs):
     instance.profile.save()
+    instance.account.save()
 
---
+++ harvestsaver/payment/models.py
@@ -0,0 +1,46 @@
+import random
+import uuid
+
+from django.db import models
+from accounts.models import User
+
+
+class Account(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    account_name = models.CharField(max_length=100)
+    account_number = models.CharField(max_length=200, unique=True)
+    account_balance = models.DecimalField(max_digits=12, decimal_places=2,
+                                          default=1000000)
+    opening_date = models.DateTimeField(auto_now_add=True)
+
+    last_transaction_date = models.DateTimeField(null=True)
+    total_withdraw = models.DecimalField(max_digits=12, decimal_places=4, default=0)
+    total_deposit = models.DecimalField(max_digits=12, decimal_places=4, default=0)
+    total_transfar = models.DecimalField(max_digits=12, decimal_places=4, default=0)
+    total_paybil = models.DecimalField(max_digits=12, decimal_places=4, default=0)
+    total_trans_amount = models.DecimalField(max_digits=12, decimal_places=4, default=0)
+
+    class Meta:
+        verbose_name = "Account"
+        verbose_name_plural = "Accounts"
+        ordering = ("-pk",)
+
+    def __str__(self):
+        return f"{self.user.username} {self.account_number}"
+
+
+def accounts_number(user_id):
+     
+	string_part = "ACC"
+	randd = random.randint(1, 10)
+	
+	uuid_part = str(uuid.uuid4().hex[:8])
+	user_num = str(int(user_id) - sum(ord(char) for char in str(user_id))).replace("-", "")
+	user_num = int(user_num)
+	user_num = str(user_num - randd)
+
+	bank_account_number = string_part + uuid_part + user_num
+
+	ciphertext = bank_account_number.upper()
+
+	return ciphertext
