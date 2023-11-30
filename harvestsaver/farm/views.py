210038a217f342930d1777f3f79a4755338bf315
Dennis Musembi Mwendwa <dennismusembi2@gmail.com>
2023-11-30 10:47:23 +0300

    added pagination for all products

--- harvestsaver/farm/views.py
+++ harvestsaver/farm/views.py
@@ -1,12 +1,11 @@
 from decimal import Decimal
-from datetime import timedelta
 from django.shortcuts import render, redirect, get_object_or_404
 from django.db.models import F, Q, Sum
 from django.db import IntegrityError
 from django.contrib import messages
 from django.core.mail import send_mail
 from django.contrib.auth.decorators import login_required
-from django.utils import timezone
+from django.core.paginator import Paginator
 
 from payment.models import order_payment
 from .models import Category, Product, Cart, Order, OrderItem
@@ -24,8 +23,17 @@ def home(request):
 
 
 def all_products(request):
+    products = Product.objects.all()
+
+    products_per_page = 1
+    page_number = request.GET.get("page")
+    paginator = Paginator(products, products_per_page)
+
+    page_object = paginator.get_page(page_number)
     
-    context = {}
+    context = {
+        "page_object": page_object,
+    }
     return render(request, "farm/all_products.html", context)
 
 def all_equipments(request):
@@ -229,7 +237,11 @@ def search(request):
             Q(name__icontains=query) | Q(description__icontains=query) |
             Q(price__icontains=query)
         )
-        return render(request, "farm/search.html", {"results": results})
+        context = {
+            "results": results,
+            "query": query,
+        }
+        return render(request, "farm/search.html", context)
 
     context = {}
     return render(request, "farm/search.html", context)
