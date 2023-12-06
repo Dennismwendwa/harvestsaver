5e1e9089d648ce837c22035ee11029790d3e2810
Dennis Musembi Mwendwa <dennismusembi2@gmail.com>
2023-12-06 11:05:05 +0300

    strip

--- harvestsaver/harvestsaver/settings.py
+++ harvestsaver/harvestsaver/settings.py
@@ -149,6 +149,18 @@ STRIPE_PUBLIC_KEY = os.environ.get("STRIPE_PUBLIC_KEY")
 STRIPE_SECRET_KEY = os.environ.get("STRIPE_SECRET_KEY")
 STRIPE_WEBHOOK_SECRET = os.environ.get("STRIPE_WEBHOOK_SECRET")
 
+# setting
+"""
+AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
+AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME")
+AWS_S3_SIGNATURE_NAME = os.environ.get("AWS_S3_SIGNATURE_NAME")
+AWS_S3_REGION_NAME = os.environ.get("AWS_S3_REGION_NAME")
+AWS_S3_FILE_OVERWRITE = os.environ.get("AWS_S3_FILE_OVERWRITE")
+AWS_DEFAULT_ACL = None
+AWS_S3_VERITY = os.environ.get("AWS_S3_VERITY")
+DEFAULT_FILE_STORAGE = os.environ.get("DEFAULT_FILE_STORAGE")
+"""
 if DEBUG:
     YOUR_DOMAIN = "http://127.0.0.1:8000"
     DATABASES = {
@@ -159,7 +171,7 @@ if DEBUG:
         }
 else:
     SECRET_KEY = os.environ.get("SECRET_KEY")
-    YOUR_DOMAIN = "http://54.237.207.136"
+    YOUR_DOMAIN = "https:pysoftware.tech"
 
 
 # Default primary key field type
